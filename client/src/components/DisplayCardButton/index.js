c48183f7a5a03ef2b0210686eca8d7025e33ff2f
PaulLeqx <lequeux.paul2@laposte.net>
2021-08-29 12:14:39 +0200

    Front - Carousel

--- client/src/components/DisplayCardButton/index.js
+++ client/src/components/DisplayCardButton/index.js
@@ -1,10 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import './index.scss';
 
 const DisplayCardButton = ({restaurant}) => {
 
   const [displayCard, setDisplayCard] = useState(false);
+
+  const [current, setCurrent] = useState(0);
+  const length = restaurant.pics.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      nextSlide();
+    }, 4000)
+  }, [current]);
+
+  if (!Array.isArray(restaurant.pics) || restaurant.pics.length <= 0) {
+    return null;
+  }
+
 
   return (
     <>
@@ -21,15 +39,41 @@ const DisplayCardButton = ({restaurant}) => {
         >
           x
         </button>
-        <div>
-          <img src="" alt="" />
+        {/* Double map because of classnames and display none on slide div that affect slide_poisition div if in the same map */}
+        <div className="carousel">
+          {restaurant.pics.map((pic, index) => {
+            return (
+              <>
+                <div
+                  className={index === current ? 'slide active' : 'slide'}
+                  key={index}
+                >
+                  {index === current && (
+                    <img className="card_img" src={pic} alt="" />
+                  )}
+                </div>
+              </>
+            );
+          })}
+        </div>
+        <div className="slide_position">
+          {restaurant.pics.map((pic, index) => {
+            return (
+              <p 
+                className={index === current ? 'slide_position-active' : 'slide_position-inactive'}
+                key={index}
+              >
+                &#9679;
+              </p>
+            );
+          })}
         </div>
         <div>
           <h2>{restaurant.name}</h2>
           <h3>{restaurant.spec}</h3>
           <p>{restaurant.address}</p>
           <div>
-            <ul>
+            <ul className="spec_list">
               <li><img src="" alt="" />{restaurant.tags[0]}</li>
               <li><img src="" alt="" />{restaurant.tags[1]}</li>
               <li><img src="" alt="" />{restaurant.tags[2]}</li>
