fc03e14ba592f13648a7156ac3827c9bf5a1be04
PaulLeqx <lequeux.paul2@laposte.net>
2021-08-29 11:02:39 +0200

    Front - Display Card

--- client/src/components/DisplayCardButton/index.js
+++ client/src/components/DisplayCardButton/index.js
@@ -1,21 +1,44 @@
+import { useState } from 'react';
+
 import './index.scss';
-import Card from '../Card';
 
 const DisplayCardButton = ({restaurant}) => {
 
-  const displayRestaurantCard = (e) => {
-    console.log(restaurant);
-    const card = e.target.nextElementSibling;
-    console.log(card);
-    card.style.display = 'block';
-  }
+  const [displayCard, setDisplayCard] = useState(false);
 
   return (
     <>
-      <button onClick={(e) => displayRestaurantCard(e)}>
+      <button 
+        onClick={() => setDisplayCard(!displayCard)}
+        className="button_display"
+      >
         {restaurant.name}
       </button>
-      <Card restaurant={restaurant} />
+      <div className={`card ${displayCard ? "show" : ""}`}>
+        <button 
+          onClick={() => setDisplayCard(!displayCard)}
+          className="button_close"
+        >
+          x
+        </button>
+        <div>
+          <img src="" alt="" />
+        </div>
+        <div>
+          <h2>{restaurant.name}</h2>
+          <h3>{restaurant.spec}</h3>
+          <p>{restaurant.address}</p>
+          <div>
+            <ul>
+              <li><img src="" alt="" />{restaurant.tags[0]}</li>
+              <li><img src="" alt="" />{restaurant.tags[1]}</li>
+              <li><img src="" alt="" />{restaurant.tags[2]}</li>
+              <li><img src="" alt="" />{restaurant.tags[3]}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className={`overlay ${displayCard ? "show" : ""}`}></div>
     </>
   );
 };
